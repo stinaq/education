7e64f642a1d82a7628e4cdf2d518c8dc27c74726
Stina Qvarnström <stinaqv@gmail.com>
2014-03-16 15:50:42 +0100

    App now shows your answer and the correct answer

--- JavaScript/quizapp/quizapp.js
+++ JavaScript/quizapp/quizapp.js
@@ -1,70 +1,81 @@
 (function (window, $) {
-    "use strict";
+  "use strict";
+  var quizData = [];
 
-    var buildQuestionMarkup = function (questionItem, index) {
-        return [
-            '<div class="question-wrapper" id="', index, '">',
-                '<p>',
-                    questionItem.question,
-                '</p>',
-                '<div>',
-                    '<div>',
-                        '<input type="checkbox" value="A">',
-                        questionItem.a,
-                    '</div>',
-                    '<div>',
-                        '<input type="checkbox" value="B">',
-                        questionItem.b,
-                    '</div>',
-                    '<div>',
-                        '<input type="checkbox" value="C">',
-                        questionItem.c,
-                    '</div>',
-                    '<div>',
-                        '<input type="checkbox" value="D">',
-                        questionItem.d,
-                    '</div>',
-                '</div>',
-                '<div>',
-                    '<input type="submit" value="Check">',
-                '</div>',
-            '</div>'
-        ].join('');
-    };
+  var buildQuestionMarkup = function (questionItem, index) {
+    return [
+      '<div class="question-wrapper" id="', index, '">',
+        '<p>',
+          questionItem.question,
+        '</p>',
+        '<div>',
+          '<div>',
+            '<input type="checkbox" value="A">',
+            questionItem.a,
+        '</div>',
+        '<div>',
+            '<input type="checkbox" value="B">',
+            questionItem.b,
+        '</div>',
+        '<div>',
+            '<input type="checkbox" value="C">',
+            questionItem.c,
+        '</div>',
+        '<div>',
+          '<input type="checkbox" value="D">',
+            questionItem.d,
+        '</div>',
+      '</div>',
+      '<div>',
+        '<input type="submit" value="Check my answer">',
+      '</div>',
+    '</div>'
+    ].join('');
+  };
 
-    var addEvents = function (quizWrapper) {
-        quizWrapper.on('click', 'input[type=submit]', function () {
-            console.log('i was clicked');
-        });
-    };
+  var checkAnswer = function (index, answers) {
+    alert('Your answer was ' + answers + ' and the correct answers were ' + quizData[index].answer);
+  };
 
-    var renderQuestion = function (markup) {
-        var quizWrapper = $('#quiz-wrapper');
-        quizWrapper.html(markup);
-        addEvents(quizWrapper);
-    };
+  var addEvents = function (quizWrapper) {
+    quizWrapper.on('click', 'input[type=submit]', function (event) {
+      var questionElement = window.bla = $(event.currentTarget).parents('.question-wrapper');
+      var questionIndex = questionElement.attr('id');
+      var answers = $.map(questionElement.find('input[type=checkbox]:checked'), function (inputElement, key) {
+        return $(inputElement).val();
+      });
+      checkAnswer(questionIndex, answers);
+    });
+  };
 
-    var success = function (data) {
-        var finishedMarkup = '';
-        $.each(data, function(index, element) {
-            finishedMarkup += buildQuestionMarkup(element, index);
-        });
-        renderQuestion(finishedMarkup);
-    };
+  var renderQuestion = function (markup) {
+    var quizWrapper = $('#quiz-wrapper');
+    quizWrapper.html(markup);
+    addEvents(quizWrapper);
+  };
 
-    var getData = function() {
-        $.ajax({
-           url: 'data.json',
-           success: success,
-           error: console.log,
-           dataType: 'json'
-        });
-    };
+  var success = function (data) {
+    var finishedMarkup = '';
+    quizData = data;
+    $.each(quizData, function(index, element) {
+        finishedMarkup += buildQuestionMarkup(element, index);
+    });
+    renderQuestion(finishedMarkup);
+  };
 
-    var initialize = function () {
-        getData();
-    };
+  var getData = function() {
+    $.ajax({
+      url: 'data.json',
+      success: success,
+      error: console.log,
+      dataType: 'json'
+    });
+  };
 
-    initialize();
+  var initialize = function () {
+    getData();
+  };
+
+  initialize();
     
 })(window, jQuery);
