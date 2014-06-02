fad808014c893a0c15d53c66004315552ca6681c
Stina Qvarnström <stinaqv@gmail.com>
2014-06-02 23:15:21 +0200

    Initial commit on Angular seed

---
+++ AngularJS/Basic-seed/app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('basicSeedApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.test = 'Stina';
+  });
