c1af7563c3bc333d47017f86633a5e9c3f337480
Stina Qvarnström <stinaqv@gmail.com>
2016-05-15 09:54:57 +0200

    Lecture updates

---
+++ WebDevelopment/3 - JavaScript/javascript-1.js
@@ -0,0 +1,12 @@
+function clickHello () {
+  alert('Hello');
+}
+
+var myButton = document.getElementById('my-button');
+myButton.addEventListener('click', clickHello);
+
+
+var changeColorButton = document.getElementById('change-color');
+changeColorButton.addEventListener('click', function () {
+  
+});
