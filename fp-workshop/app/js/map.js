5ea5dbe9d5799351688c9514a8d5abe552e75406
Stina Qvarnström <stina@stinaq.se>
2017-11-19 15:10:03 +0100

    Implemented map and tests for map

--- fp-workshop/app/js/map.js
+++ fp-workshop/app/js/map.js
@@ -1,7 +1,12 @@
 const ada = {};
 
-function map(originalArray) {
-  return ['a', 'b'];
+function map(originalArray, callback) {
+  let newArray = [];
+  originalArray.forEach(function(item, index) {
+    originalArray[index] = callback(item);
+    // newArray.push(callback(item));
+  });
+  return originalArray;
 }
 
 ada.map = map;
--- fp-workshop/jasmine/spec/map.test.js
+++ fp-workshop/jasmine/spec/map.test.js
@@ -1,21 +1,63 @@
 describe("Map", function() {
   describe('basic functionality', () => {
     let result;
-    const startArray = ['a', 'b'];
+    const mockFunction = jasmine.createSpy('mockFunction');
+    const startArray = ['Diana', 'Hippolyta'];
     beforeEach(() => {
-      result = ada.map(startArray);
+      result = ada.map(startArray, mockFunction);
     });
     it('should return an array', () => {
       expect(result).toEqual(jasmine.any(Array));
     });
+    it('should return an array as long as the one given', () => {
+      expect(result.length).toEqual(2);
+    });
+    it('should call the callback function', () => {
+      expect(mockFunction).toHaveBeenCalled();
+    });
+  });
+  describe('given an array of length 3', function() {
+    let result;
+    const mockFunction = jasmine.createSpy('mockFunction');
+    const startArray = ['Ada Lovelace', 'Margeret Hamilton', 'Grace Hopper'];
+    beforeEach(() => {
+      result = ada.map(startArray, mockFunction);
+    });
+    it('should call the callback function 3 times', () => {
+      expect(mockFunction.calls.count()).toEqual(3);
+    });  
+    it('should return an array as long as the one given', () => {
+      expect(result.length).toEqual(3);
+    }); 
+  });
+  describe('given a callback that increases the numbers by 2 ', () => {
+    let result;
+    const increaserCallback = function(item) {
+      return item + 2;
+    }
+    const startArray = [2, 5, 7, 9, 6];
+    beforeEach(() => {
+      result = ada.map(startArray, increaserCallback);
+    });
+    it('should return an array where the callback has been applied to each item in the array', () => {
+      const expectedResult = [4, 7, 9, 11, 8];
+      expect(result).toEqual(expectedResult);
+    });
+  });
+  describe('functional programming functionality', () => {
+    let result;
+    const salaryIncreaser = function(salary) {
+      return salary * 1.25;
+    };
+    const startArray = [38000, 45000];
+    beforeEach(() => {
+      result = ada.map(startArray, salaryIncreaser);
+    });
     it('should not return the same array as given', () => {
       expect(result).not.toBe(startArray);
     });
-  });
-  describe('should return an array', function() {
-    // it('something', function() {
-    //   var result = ada.map();
-    //   expect(result).toEqual(2);
-    // })    
+    it('should not have modified the given array', () => {
+      expect(startArray).toEqual([38000, 45000]);
+    });
   });
 });
